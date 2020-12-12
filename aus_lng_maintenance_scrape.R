519fe7d27524653c5b01d8eb01304c8a93504592
jonfoong <jonahfoong@gmail.com>
2020-12-13 09:18:26 +1000

    cleaning lines

--- aus_lng_maintenance_scrape.R
+++ aus_lng_maintenance_scrape.R
@@ -1,7 +1,6 @@
 library(rvest)
 library(pushoverr)
 library(tidyverse)
-library(googlesheets4)
 
 # Set Aus time
 Sys.setenv(TZ='GMT-10')
@@ -17,12 +16,6 @@ y<-read_html(url) %>% html_nodes('#content > div > div > div > div > div > div.c
 z<-read_html(url) %>% html_nodes('#content > div > div > div > div > div > div.col-12.col-lg-8.layout-col > div.component.file-list > div > ul > li:nth-child(3) > a > div > div.field-updated.field-publisheddate > span') %>%
   as.character() %>% str_extract(.,"\\d+/\\d+/\\d+")
 
-#check google sheet
-gs4_deauth()
-update<-read_sheet("https://docs.google.com/spreadsheets/d/1GR_xobGPzgXLBb5enjXS8toEUqmlEejeoGjx052ot7I/edit?usp=sharing",4,col_names = TRUE)
-
-if (update$Maintenance=='not updated'){
-
 if (format(Sys.Date(),"%d/%m/%Y")==x){
   pushover(message='New maintenance entry!', 
            user="uccrmx7ajshvdsgbx2e2qy17eorpsx", 
@@ -37,5 +30,4 @@ if (format(Sys.Date(),"%d/%m/%Y")==z){
   pushover(message='Third maintenance entry today!', 
            user="uccrmx7ajshvdsgbx2e2qy17eorpsx", 
            app="akhzmh5yoco7koy31oos1micwsbxh7")
-}
 }
